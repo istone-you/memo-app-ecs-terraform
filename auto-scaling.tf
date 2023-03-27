4d50c1943749b26491bd00a6b61c406744cc449a
istone-you <istoneyou1205@yahoo.co.jp>
2023-03-27 07:27:03 +0000

    ファイル分割

---
+++ auto-scaling.tf
@@ -0,0 +1,59 @@
+
+resource "aws_autoscaling_group" "frontend" {
+  name             = "ecs-frontend-asg"
+  min_size         = 1
+  max_size         = 1
+  desired_capacity = 1
+
+  launch_configuration = aws_launch_configuration.ecs.name
+  vpc_zone_identifier  = ["subnet-09d45d5b0722e6315"]
+
+  service_linked_role_arn = "arn:aws:iam::763397213391:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+
+  tags = [
+    {
+      key                 = "AmazonECSManaged"
+      propagate_at_launch = true
+      value               = ""
+    }
+  ]
+}
+
+resource "aws_autoscaling_group" "backend" {
+  name             = "ecs-backend-asg"
+  min_size         = 1
+  max_size         = 1
+  desired_capacity = 1
+
+  launch_configuration = aws_launch_configuration.ecs.name
+  vpc_zone_identifier  = ["subnet-09d45d5b0722e6315"]
+
+  service_linked_role_arn = "arn:aws:iam::763397213391:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling"
+
+  tags = [
+    {
+      key                 = "AmazonECSManaged"
+      propagate_at_launch = true
+      value               = ""
+    }
+  ]
+}
+
+resource "aws_launch_configuration" "ecs" {
+  name_prefix   = "ecs-launch-tf-"
+  image_id      = "ami-09c4258ce9b81642c"
+  instance_type = "t2.medium"
+
+  security_groups = [
+    "sg-00e7cdcedbd89dc4a"
+  ]
+  enable_monitoring    = true
+  iam_instance_profile = "arn:aws:iam::763397213391:instance-profile/ECS-SSM"
+
+  user_data = <<EOF
+#!/bin/bash
+echo ECS_CLUSTER=mern >> /etc/ecs/ecs.config
+EOF
+
+  associate_public_ip_address = true
+}
